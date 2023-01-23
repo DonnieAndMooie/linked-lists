e50dcea3bc16d93c8874c4ae1b17334922eda01e
DonnieAndMooie <owenjeaves@gmail.com>
2023-01-23 14:10:06 +0000

    Create function append(value) which adds to end of list

--- linked-list.js
+++ linked-list.js
@@ -1,7 +1,28 @@
 const linkedListFactory = (list) => {
-    return {list};
+    const append = (value) => {
+        if (!list.head){
+            list.head = nodeFactory(value, null)
+        }
+        else{
+            let currentNode = list.head
+            while (currentNode.nextNode){
+                currentNode = currentNode.nextNode
+            }
+            newNode = nodeFactory(value, null)
+            currentNode.nextNode = newNode
+        }
+    } 
+    return {list, append};
 }
 
 const nodeFactory = (value, nextNode) => {
     return {value, nextNode};
 }
+
+
+const list = linkedListFactory({})
+list.append(120)
+list.append(140)
+list.append(200)
+console.log(list.list)
+
